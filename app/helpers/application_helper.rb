a3a0511434bddc4d28dee5e97359fb4f68dd74be
chan <baguschandradp@gmail.com>
2023-02-21 16:09:36 +0800

    add gem and controller

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,13 @@
+module ApplicationHelper
+  include Pagy::Frontend
+
+  def sort_link_to(name, column, **options)
+    direction = if params[:sort] == column.to_s
+                  params[:direction] == 'asc' ? 'desc' : 'asc'
+                else
+                  'asc'
+                end
+
+    link_to name, request.params.merge(sort: column, direction:), **options
+  end
+end
